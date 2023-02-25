53ce66b61645cb1e61180bce93fb798f05cb877d
simjieyu95 <simjieyu95@gmail.com>
2023-02-25 14:10:39 +0800

    InsuranceClaim
    insuranceClaimStuff

--- backend/controllers/insuranceClaimsController.js
+++ backend/controllers/insuranceClaimsController.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import Claims from "../database/models/insuranceClaims.js";
 import Policies from "../database/models/policies.js";
 
@@ -15,6 +16,30 @@ const findClaims = async (req, res) => {
         }) 
 
 
+=======
+const loginUser = async (req, res) => {
+    try {
+        const { error } = validateSignIn(req.body);
+        if (error)
+            return res.status(400).send({ message: error.details[0].message });
+
+        // Check for loginID / email.
+        const user = await User.findOne({ email: req.body.email });
+        if (!user)
+            return res
+                .status(401)
+                .send({ message: "Invalid Email or Password" });
+
+        // Check for password
+        const validPassword = await bcrypt.compare(
+            req.body.password,
+            user.password
+        );
+        if (!validPassword)
+            return res
+                .status(401)
+                .send({ message: "Invalid Email or Password" });
+>>>>>>> Stashed changes
 
         // Generate a authentication token and returns to the user.
         const token = user.generateAuthToken(req.body.email);
---
+++ backend/database/models/insuranceClaims.js
@@ -0,0 +1,23 @@
+import crypto from "crypto";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+const claims = new mongoose.Schema({
+    // loginID: { type: String, required: true },
+    claimID: { type: String, required: true, unique: true },
+    insuranceID: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    expenseDate: { type: Date }, // Token used to validate when resetting password.
+    amount: { type: Number }, // Expiration date for resetting password.
+    purpose:{ type: String},
+    followUp: { type: Boolean},
+    previousClaimID:{ type: Number },
+    status:{ type: String},
+    lastEditedClaimDate:{ type: String},
+    receiptNo:{ type: Number, required: true},
+});
+
+
+const Claims = mongoose.model("claims", userSchema);
+export default Claims;
