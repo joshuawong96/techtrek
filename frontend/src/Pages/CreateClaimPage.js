2a309f8ab1981853df0406a1474389dfe88695c6
nowknowing <s.sunaga00@gmail.com>
2023-02-25 11:20:52 +0800

    add empty createClaimPage

---
+++ frontend/src/Pages/CreateClaimPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const CreateClaimPage = () => {
+    return (
+        <div>
+        <h1>Create Claim Page</h1>
+        </div>
+    );
+    };
+
+    export default CreateClaimPage;
