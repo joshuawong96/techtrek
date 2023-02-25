f899ec03e1748f6bb8463b9ca81d593dc560624e
eggyeolk <amandatan2000@gmail.com>
2023-02-25 15:11:21 +0800

    change to list

--- backend/controllers/policyController.js
+++ backend/controllers/policyController.js
@@ -6,7 +6,12 @@ import Policy from "../database/models/policies.js";
 const getPolicy = async (req, res) => {
     
     const get_policies = await Policy.find({ employeeID: 58001002 }).select({ "insuranceID": 1, "_id": 0});
-    console.log(get_policies)
+    const ID_list = [];
+
+    for (let i=0; i < get_policies.length; i++) {
+        ID_list[i] = get_policies[i].insuranceID;
+    }
+    console.log(ID_list)
     //return get_policies
     //req.body.employeeID
 }
