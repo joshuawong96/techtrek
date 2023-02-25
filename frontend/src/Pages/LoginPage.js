fd0cfeea3a1e446d68fa358045200f3c44f1e188
insafhere <insaf@u.nus.edu>
2023-02-25 11:12:50 +0800

    login page

---
+++ frontend/src/Pages/LoginPage.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+const LoginPage = () => {
+  return (
+    <div className="form">
+        <h1>
+            LOGIN PAGE
+        </h1>
+      </div>
+  )
+}
+
+export default LoginPage
